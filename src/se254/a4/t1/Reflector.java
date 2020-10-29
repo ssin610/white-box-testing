f1d03830b1f15aef489b230aaf842aba24bc67e9
ssin610 <ssin610@aucklanduni.ac.nz>
2020-10-30 12:55:27 +1300

    [docs] Added documentation in the Reflector class for the new methods.
    Added JavaDoc commenting to each method in the Reflector class to help
    explain the purpose of the method.

--- src/se254/a4/t1/Reflector.java
+++ src/se254/a4/t1/Reflector.java
@@ -26,7 +26,8 @@ public class Reflector {
                                                                        // user to select
 
     /**
-     * Creates a Reflector instance with a class for reflection
+     * Creates a Reflector instance with a class for reflection and creates an
+     * instance of the input class
      * 
      * @param inputClass the class for reflection
      */
@@ -47,8 +48,9 @@ public class Reflector {
     }
 
     /**
-     * Does reflection on the input class by accessing methods/fields and also takes
-     * user input for which method to execute
+     * Does reflection on the input class by calling methods to print the fields and
+     * methods of the class. Also takes in user input for which method to invoke on
+     * the object
      */
     public void reflect() {
         int num = -1; // variable to track the user input
@@ -58,46 +60,58 @@ public class Reflector {
         // check if user has pressed 0 to quit
         while (num != 0) {
 
+            // call method to print fields
             try {
                 printFields();
             } catch (IllegalAccessException e) {
                 System.out.println("Error: Could not access the fields");
             }
 
+            // call method to print methods of the class
             printMethods();
 
+            // get user input for which method to invoke
             Scanner sc = new Scanner(System.in);
-            // get user input
+
             System.out.print("Enter the number corresponding to the method you want to execute. Enter 0 to quit: ");
             num = sc.nextInt();
             System.out.println(
-                "==============================================================================================================");
-           
+                    "==============================================================================================================");
+
+            // check if user has pressed 0 to quit
             if (num == 0) {
                 break;
             }
-            
+
+            // invoke the method specified by the user
             try {
-                map.get(num).invoke(_object);
+                invokeMethod(num);
             } catch (IllegalAccessException e) {
                 System.out.println("Error: Could not access the method");
-            }  catch (InvocationTargetException e) {
+            } catch (InvocationTargetException e) {
                 System.out.println("Error: Could not invoke the method");
             }
         }
     }
 
+    /**
+     * This method prints the public fields and their corresponding values for the
+     * input class
+     */
     public void printFields() throws IllegalArgumentException, IllegalAccessException {
-        // list the fields and methods of the class, where each method has a number
-        // corresponding to it
+
         for (Field f : _inputClass.getFields()) {
             System.out.println("Field name = " + f.getName() + ", Field value = " + f.get(_object));
         }
     }
 
+    /**
+     * This method prints the public methods which take no parameters for the input
+     * class
+     */
     public void printMethods() {
         int count = 1;
-       
+
         for (Method m : _inputClass.getDeclaredMethods()) {
             if (m.getParameterTypes().length == 0 && m.getModifiers() == Modifier.PUBLIC) {
                 map.put(count, m);
@@ -107,9 +121,13 @@ public class Reflector {
         }
     }
 
+    /**
+     * This method invokes a method specified by the user on the object of the input
+     * class
+     * @param num the number corresponding to the method
+     */
     public void invokeMethod(int num)
             throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         map.get(num).invoke(_object);
     }
-
 }
